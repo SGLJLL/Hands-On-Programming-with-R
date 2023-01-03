648e97f689d3ae9293f7377e230158112715928e
SogolJalali <99341775+SGLJLL@users.noreply.github.com>
2023-01-03 23:39:25 +0330

    Add files via upload

---
+++ 1.R
@@ -0,0 +1,17 @@
+# Choose any number and assign it to a variable
+my_number <- 4
+
+# Add 2 to the number
+result1 <- my_number + 2
+
+# Multiply the result by 3
+result2 <- result1 * 3
+
+# Subtract 6 from the result
+result3 <- result2 - 6
+
+# Divide the result by 3
+result4 <- result3 / 3
+
+# Print the final result
+result4
